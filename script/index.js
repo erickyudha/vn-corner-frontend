8fd9e40a1caf203c33dd98cb9f9d270ae7f274fe
Erick Yudha <erickyudha18@gmail.com>
2021-05-16 16:22:05 +0700

    page1 section1 done

---
+++ script/index.js
@@ -0,0 +1,10 @@
+new fullpage('#fullpage', {
+  navigation: true,
+  responsiveWidth: 700,
+  anchors: ['reccomendation', 'about-us', 'contact'],
+  parallax: true,
+  sectionsColor: ['#FFF', '#FFF', '#FFF', '#FFF'],
+  onLeave: function(origin, destination, direction){
+      console.log("Leaving section" + origin.index);
+  },
+});
